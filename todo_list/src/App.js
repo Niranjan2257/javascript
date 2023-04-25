403f97aff08b7fca6a002f7832b1f42268bfc526
Niranjan <niranjan2257@gmail.com>
2023-04-25 09:05:56 +0530

    1st commit

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Niranjan nishuosh
         </p>
         <a
           className="App-link"

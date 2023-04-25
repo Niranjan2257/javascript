0adc9bd53f72e93fa309fe0d970fc4cdfdc211ec
Niranjan <niranjan2257@gmail.com>
2023-04-25 11:33:28 +0530

    mod app.js

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Niranjan nishuosh
+          Niranjan kumar nishuosh
         </p>
         <a
           className="App-link"

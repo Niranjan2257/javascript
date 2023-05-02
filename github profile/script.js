c7609a6d08903ae279b3e07dda35b662fd38c873
Niranjan <niranjan2257@gmail.com>
2023-05-02 20:59:04 +0530

    input for username

--- github profile/script.js
+++ github profile/script.js
@@ -1,63 +1,73 @@
-const username = "Niranjan2257";
-const apiUrl = `https://api.github.com/users/${username}`;
+// DOM elements
+const usernameInput = document.getElementById("username-input");
+const submitButton = document.getElementById("submit");
 
-// Fetch user profile data
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const profileElement = document.getElementById("profile");
+// Event listener for submit button
+submitButton.addEventListener("click", fetchUserData);
 
-    // Create profile HTML
-    const profileHTML = `
-      <img src="${data.avatar_url}" alt="Profile Picture" width="100" height="100">
-      <h2>${data.name}</h2>
-      <p>${data.login}</p>
-      <p>${data.bio}</p>
-      <p>Followers: ${data.followers}</p>
-      <p>Following: ${data.following}</p>
-      <p>Public Repositories: ${data.public_repos}</p>
-    `;
+// Function to fetch user data
+function fetchUserData() {
+  const username = usernameInput.value.trim();
+  const apiUrl = `https://api.github.com/users/${username}`;
 
-    profileElement.innerHTML = profileHTML;
-  })
-  .catch(error => {
-    const errorMessage = document.createElement("p");
-    errorMessage.classList.add("error-message");
-    errorMessage.textContent = "Error fetching profile data.";
+  // Fetch user profile data
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const profileElement = document.getElementById("profile");
 
-    const profileElement = document.getElementById("profile");
-    profileElement.appendChild(errorMessage);
-    console.error(error);
-  });
-
-// Fetch user repositories
-fetch(`${apiUrl}/repos`)
-  .then(response => response.json())
-  .then(data => {
-    const repositoriesElement = document.getElementById("repositories");
-
-    // Create repositories HTML
-    let repositoriesHTML = "";
-    data.forEach(repository => {
-      repositoriesHTML += `
-        <div class="repository">
-          <h2>${repository.name}</h2>
-          <p>${repository.description}</p>
-          <p>Language: ${repository.language}</p>
-          <p>Stars: ${repository.stargazers_count}</p>
-          <p>Contributions: ${repository.contributions}</p>
-        </div>
+      // Create profile HTML
+      const profileHTML = `
+        <img src="${data.avatar_url}" alt="Profile Picture" width="100" height="100">
+        <h2>${data.name}</h2>
+        <p>${data.login}</p>
+        <p>${data.bio}</p>
+        <p>Followers: ${data.followers}</p>
+        <p>Following: ${data.following}</p>
+        <p>Public Repositories: ${data.public_repos}</p>
       `;
+
+      profileElement.innerHTML = profileHTML;
+    })
+    .catch(error => {
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error-message");
+      errorMessage.textContent = "Error fetching profile data.";
+
+      const profileElement = document.getElementById("profile");
+      profileElement.appendChild(errorMessage);
+      console.error(error);
     });
 
-    repositoriesElement.innerHTML = repositoriesHTML;
-  })
-  .catch(error => {
-    const errorMessage = document.createElement("p");
-    errorMessage.classList.add("error-message");
-    errorMessage.textContent = "Error fetching repositories.";
+  // Fetch user repositories
+  fetch(`${apiUrl}/repos`)
+    .then(response => response.json())
+    .then(data => {
+      const repositoriesElement = document.getElementById("repositories");
 
-    const repositoriesElement = document.getElementById("repositories");
-    repositoriesElement.appendChild(errorMessage);
-    console.error(error);
-  });
+      // Create repositories HTML
+      let repositoriesHTML = "";
+      data.forEach(repository => {
+        repositoriesHTML += `
+          <div class="repository">
+            <h2>${repository.name}</h2>
+            <p>${repository.description}</p>
+            <p>Language: ${repository.language}</p>
+            <p>Stars: ${repository.stargazers_count}</p>
+            <p>Contributions: ${repository.contributions}</p>
+          </div>
+        `;
+      });
+
+      repositoriesElement.innerHTML = repositoriesHTML;
+    })
+    .catch(error => {
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error-message");
+      errorMessage.textContent = "Error fetching repositories.";
+
+      const repositoriesElement = document.getElementById("repositories");
+      repositoriesElement.appendChild(errorMessage);
+      console.error(error);
+    });
+}

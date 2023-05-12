59ee86ffd488dc31d32a1471c76db0d53dd3d5be
Niranjan <niranjan2257@gmail.com>
2023-05-12 07:57:20 +0530

    contact added

--- New folder/script.js
+++ New folder/script.js
@@ -7,3 +7,11 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  function openContact() {
+    document.getElementById("contact_window").style.width = "100%";
+  }
+  
+  function closeContact() {
+    document.getElementById("contact_window").style.width = "0";
+  }
